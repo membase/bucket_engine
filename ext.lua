1837236ddffae5cafdefeccf6d5c28f237b381ef
Steve Yen <steve.yen@gmail.com>
2010-12-01 01:16:44 -0800

    nget and nstore example counters
    Change-Id: I0fd6b3bbdcaa9c4a4788c17b27b583fe7b290d1b

--- ext.lua
+++ ext.lua
@@ -1,8 +1,23 @@
 local nget = 0
+local nstore = 0
 
 function engine_get(engine, cookie, key, vbucket)
   if key == "nget" then
      data = tostring(nget)
+     rv, item = engine:allocate(cookie,
+                                key, string.len(data), 0, 0)
+     if rv == 0 then
+       rv = engine:set_item_data(cookie, item, data)
+       if rv == 0 then
+         return 0, item
+       end
+     end
+
+     return 1
+  end
+
+  if key == "nstore" then
+     data = tostring(nstore)
      rv, item = engine:allocate(cookie,
                                 key, string.len(data), 0, 0)
      if rv == 0 then
@@ -22,6 +37,8 @@ function engine_get(engine, cookie, key, vbucket)
 end
 
 function engine_store(engine, cookie, item, cas, operation, vbucket)
+  nstore = nstore + 1
+
   -- log(5, "lua ENGINE_STORE, vbucket: " .. vbucket)
   return engine:store(cookie, item, cas, operation, vbucket)
 end
