d3db09f83debcf2b6d9d24aaac90c7b0cd5fe6db
Steve Yen <steve.yen@gmail.com>
2010-12-03 23:12:53 -0800

    example set/store operation in lua
    Change-Id: Ibc1f43fd0eaa5569dc5985e8dfbb8fe70889937c

--- ext.lua
+++ ext.lua
@@ -2,7 +2,7 @@ nget = 0
 nstore = 0
 
 function engine_get(engine, cookie, key, vbucket)
-  -- log(5, "lua ENGINE_GET for key: " .. key .. " vbucket: " .. vbucket)
+  -- log(LOG_WARNING, "lua ENGINE_GET for key: " .. key .. " vbucket: " .. vbucket)
 
   nget = nget + 1
 
@@ -58,15 +58,38 @@ function engine_get(engine, cookie, key, vbucket)
   return engine:get(cookie, key, vbucket)
 end
 
+log(LOG_WARNING, "lua ext engine_store hook")
+
 function engine_store(engine, cookie, item, cas, operation, vbucket)
-  -- log(5, "lua ENGINE_STORE vbucket: " .. vbucket)
+  -- log(LOG_WARNING, "lua ENGINE_STORE vbucket: " .. vbucket)
 
   nstore = nstore + 1
 
   if operation == OPERATION_APPEND then
-    rv, key, flags, exptime, old_cas, nbytes, data =
+    rv, key, flags, exptime, cas, nbytes, data =
       engine:get_item_data(cookie, item, 0, -1)
-    if rv == 0 and key and key:sub(0, 4) == "SET~" then
+    if (rv == 0 and
+        key and
+        key:sub(0, 4) == "SET@") then
+      rv, curr_item = engine:get(cookie, key, vbucket)
+      if rv == 0 and curr_item then
+        rv, curr_key, curr_flags, curr_exptime, curr_cas, curr_nbytes, curr_data =
+          engine:get_item_data(cookie, curr_item, 0, -1)
+        if rv == 0 then
+          if (curr_data and
+              curr_data:match(data:sub(1, -3))) then
+            return ENGINE_SUCCESS
+          else
+            return engine:store(cookie, item, cas, operation, vbucket)
+          end
+        end
+
+        return rv
+      end
+
+      if rv == ENGINE_KEY_ENOENT then
+        return engine:store(cookie, item, cas, OPERATION_ADD, vbucket)
+      end
     end
   end
 
@@ -74,7 +97,7 @@ function engine_store(engine, cookie, item, cas, operation, vbucket)
 end
 
 function engine_flush_all(engine, cookie, when)
-  -- log(5, "lua engine flush " .. engine:name(cookie))
+  -- log(LOG_WARNING, "lua engine flush " .. engine:name(cookie))
 
   return engine:flush_all(cookie, when)
 end
